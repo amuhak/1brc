7897254bb97a5caf205f92c417eb90faf52e78b1
Amulya Jain <amuly.jain@gmail.com>
2024-06-23 22:29:12 -0400

    Done 🎉

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,131 @@
 import org.graalvm.collections.Pair;
 
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
+    private static final String FILE_PATH = "measurements.txt";
+    private static final int BUFFER_SIZE = 1 << 23;
+    private static final ConcurrentHashMap<String, value> map = new ConcurrentHashMap<>();
+
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
-        FastIO sc = new FastIO("measurements.txt");
-        HashMap<String, value> map = new HashMap<>();
-        String val;
-        while ((val = sc.readLine()) != null) {
-            String[] arr = val.split(";");
-            String name = arr[0];
-            int change = (int) (Double.parseDouble(arr[1]) * 10);
-            if (!map.containsKey(name)) {
-                map.put(name, new value(0, 0, 0));
+        final long length;
+        RandomAccessFile file = new RandomAccessFile(FILE_PATH, "r");
+
+        length = file.length();
+
+        ArrayList<Pair<Long, Long>> toDo = new ArrayList<>();
+        long to;
+        for (long i = 0; i < length - 1; i = to) {
+            to = Math.min(i + BUFFER_SIZE, length - 1);
+            file.seek(to);
+            while (file.read() != '\n' && to < length) {
+                to++;
             }
-            value v = map.get(name);
-            v.update(change);
+            toDo.add(Pair.create(i, to));
         }
+
+        // System.out.println(toDo.size());
+
+        int noThreads = Runtime.getRuntime().availableProcessors();
+
+        ExecutorService service =
+                new ThreadPoolExecutor(noThreads, noThreads, 1L, java.util.concurrent.TimeUnit.SECONDS,
+                        new java.util.concurrent.LinkedBlockingQueue<>());
+
+        for (Pair<Long, Long> pair : toDo) {
+            service.submit(new Worker(pair.getLeft(), pair.getRight()));
+        }
+
+        service.shutdown();
+        while (!service.isTerminated()) {
+
+        }
+
         ArrayList<Pair<String, value>> list = new ArrayList<>();
         for (String key : map.keySet()) {
             list.add(Pair.create(key, map.get(key)));
         }
-        // Sort the list by string
+
         list.sort(Comparator.comparing(Pair::getLeft));
-        // Print like Abha=5.0/18.0/27.4
+
         StringBuilder sb = new StringBuilder();
         sb.append("{");
         for (Pair<String, value> pair : list) {
             value v = pair.getRight();
-            sb.append(pair.getLeft()).append("=")
-                    .append(v.min / 10.0)
-                    .append("/")
-                    .append(Math.round(v.sum / (double) v.n) / 10.0)
-                    .append("/")
-                    .append(v.max / 10.0)
-                    .append(", ");
+            sb.append(pair.getLeft()).append("=").append(v.min / 10.0).append("/").append(
+                    Math.round(v.sum / (double) v.n) / 10.0).append("/").append(v.max / 10.0).append(", ");
         }
         sb.setLength(sb.length() - 2);
         sb.append("}");
         System.out.println(sb);
+
+        System.out.println();
         long end = System.currentTimeMillis();
         System.out.println("Time taken: " + (end - start) + "ms");
+    }
+
+    static class Worker implements Runnable {
+        long i;
+        long to;
+
+        public Worker(long i, long to) {
+            this.i = i;
+            this.to = to;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[BUFFER_SIZE + BUFFER_SIZE / 8];
+            int len = 0;
+            try {
+                var file = new RandomAccessFile(FILE_PATH, "r");
+                file.seek(i);
+                int delta = (int) (to - i);
+                len = file.read(buffer, 0, delta);
+                buffer[len] = '\n';
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            for (int j = 0; j < len; j++) {
+                if (buffer[j] == '\n') {
+                    continue;
+                }
+                int start = j;
+                while (buffer[j] != ';') {
+                    j++;
+                }
+                String name = new String(buffer, start, j - start);
+                j++;
+                int no = 0;
+                boolean neg = false;
+                if (buffer[j] == '-') {
+                    neg = true;
+                    j++;
+                }
+                while (buffer[j] != '\n') {
+                    if (buffer[j] == '.') {
+                        j++;
+                        continue;
+                    }
+                    no *= 10;
+                    no += buffer[j] - '0';
+                    j++;
+                }
+                if (neg) {
+                    no = -no;
+                }
+                if (map.containsKey(name)) {
+                    map.get(name).update(no);
+                } else {
+                    map.put(name, new value(no));
+                }
+            }
+        }
     }
 
     public static class value {
@@ -55,137 +134,18 @@ public class Main {
         int max;
         int n;
 
-        public value(long sum, int min, int max) {
-            this.sum = sum;
-            this.min = min;
-            this.max = max;
+        public value(int n) {
+            this.sum = n;
+            this.min = n;
+            this.max = n;
             this.n = 1;
         }
 
-        public void update(int val) {
+        public synchronized void update(int val) {
             sum += val;
             min = Math.min(min, val);
             max = Math.max(max, val);
             n++;
-        }
-    }
-
-    public static class FastIO {
-        public int BUFFER_SIZE = 1 << 18;
-        private final DataInputStream din;
-        private final byte[] buffer;
-        private int bufferPointer, bytesRead;
-
-        public FastIO() {
-            din = new DataInputStream(System.in);
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
-        }
-
-        public FastIO(String file_name) throws IOException {
-            din = new DataInputStream(new FileInputStream(file_name));
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
-        }
-
-        public String readLine() throws IOException {
-            byte[] buf = new byte[64]; // line length
-            int cnt = 0, c;
-            while ((c = read()) != -1) {
-                if (c == '\n') {
-                    if (cnt != 0) {
-                        break;
-                    } else {
-                        continue;
-                    }
-                }
-                buf[cnt++] = (byte) c;
-            }
-            if (cnt == 0) {
-                return null;
-            }
-            return new String(buf, 0, cnt);
-        }
-
-        public int nextInt() throws IOException {
-            int ret = 0;
-            byte c = read();
-            while (c <= ' ') {
-                c = read();
-            }
-            boolean neg = (c == '-');
-            if (neg) {
-                c = read();
-            }
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-
-            return neg ? -ret : ret;
-        }
-
-        public long nextLong() throws IOException {
-            long ret = 0;
-            byte c = read();
-            while (c <= ' ') {
-                c = read();
-            }
-            boolean neg = (c == '-');
-            if (neg) {
-                c = read();
-            }
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-            if (neg) {
-                return -ret;
-            }
-            return ret;
-        }
-
-        public double nextDouble() throws IOException {
-            double ret = 0, div = 1;
-            byte c = read();
-            while (c <= ' ') {
-                c = read();
-            }
-            boolean neg = (c == '-');
-            if (neg) {
-                c = read();
-            }
-
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-
-            if (c == '.') {
-                while ((c = read()) >= '0' && c <= '9') {
-                    ret += (c - '0') / (div *= 10);
-                }
-            }
-
-            if (neg) {
-                return -ret;
-            }
-            return ret;
-        }
-
-        private void fillBuffer() throws IOException {
-            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
-            if (bytesRead == -1) {
-                buffer[0] = -1;
-            }
-        }
-
-        private byte read() throws IOException {
-            if (bufferPointer == bytesRead) {
-                fillBuffer();
-            }
-            return buffer[bufferPointer++];
-        }
-
-        public void close() throws IOException {
-            din.close();
         }
     }
 }
